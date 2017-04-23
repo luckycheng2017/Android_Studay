e8e1bceeb08cfe9b145b73191107ba7cc42b38d0
luckycheng <1249363595@qq.com>
2017-04-23 18:34:23 +0800

    实现背光灯调节功能

--- APP_LightDemo/app/src/main/java/com/example/wang/app_lightdemo/MainActivity.java
+++ APP_LightDemo/app/src/main/java/com/example/wang/app_lightdemo/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.wang.app_lightdemo;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.os.Handler;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mLightButton = null;
+    private SeekBar mBacklightSeekBar = null;
     private boolean flashing = false;
     final private int LED_NOTIFICATION_ID = 123;
 
@@ -50,7 +53,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLightButton = (Button) findViewById(R.id.button);
+        mBacklightSeekBar = (SeekBar) findViewById(R.id.seekbar);
 
+        try {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+
+            int brightness = android.provider.Settings.System.getInt(getContentResolver(),
+                    android.provider.Settings.System.SCREEN_BRIGHTNESS);
+            mBacklightSeekBar.setProgress(brightness*100/255);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
 
         mLightButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,10 +75,32 @@ public class MainActivity extends AppCompatActivity {
                 if (flashing) {
                     mLightButton.setText("Stop Flashing the Light");
                 } else {
-                    mLightButton.setText("Flashing Light at 20S");
+                    mLightButton.setText("Flashing Light at 65S");
                 }
 
-                mLightHander.postDelayed(mLightRunnable, 20000);
+                mLightHander.postDelayed(mLightRunnable, 65000);
+            }
+        });
+
+        mBacklightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int brightness = mBacklightSeekBar.getProgress();
+                brightness = brightness * 255 / 100;
+
+                android.provider.Settings.System.putInt(getContentResolver(),
+                        android.provider.Settings.System.SCREEN_BRIGHTNESS,
+                        brightness);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar){
+
             }
         });
     }
