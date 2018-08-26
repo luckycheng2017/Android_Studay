86008854f24b326c8dd51ffd952b0974be2c983d
luckycheng <1249363595@qq.com>
2018-08-26 09:47:24 +0800

    添加MainActivity的扫描动作

--- PriceCode/app/src/main/java/com/example/wang/pricecode/MainActivity.java
+++ PriceCode/app/src/main/java/com/example/wang/pricecode/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.wang.pricecode;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.Manifest;
+
+import com.example.wang.pricecode.zxing.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button MyScanButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyScanButton = (Button) findViewById(R.id.button);
+        MyScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                } else {
+                    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
