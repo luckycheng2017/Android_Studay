6cfb98fb34819ded5994eacdfc525ed93402b67a
luckycheng <1249363595@qq.com>
2018-08-26 21:55:10 +0800

    修改安卓样式

--- PriceCode/app/src/main/java/com/example/wang/pricecode/MainActivity.java
+++ PriceCode/app/src/main/java/com/example/wang/pricecode/MainActivity.java
@@ -2,14 +2,13 @@ package com.example.wang.pricecode;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import android.Manifest;
 
 import com.example.wang.pricecode.zxing.activity.CaptureActivity;
 
@@ -22,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyScanButton = (Button) findViewById(R.id.button);
+        MyScanButton = (Button) findViewById(R.id.scan_button);
+
         MyScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
